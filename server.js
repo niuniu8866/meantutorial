8789871306cb4c7d084dc3e3f9fa96f5cf7d05cb
Ye Feng Wen <ye.feng.wen@huawei.com>
2017-08-02 17:42:10 -0400

    Added Morgan

--- server.js
+++ server.js
@@ -2,6 +2,10 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 8080;
 
+var morgan = require('morgan');
+
+app.use(morgan());
+
 app.get('/', function(req, res){
    res.send('Hello World') ;
 });
